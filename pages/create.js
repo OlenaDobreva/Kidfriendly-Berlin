6289a5b6cb59432f0056efc51581ac438d8d4c7f
OLENA DOBREVA <sectionone@ukr.net>
2024-04-15 12:03:04 +0200

    create form logic which add new place but not redirecting

--- pages/create.js
+++ pages/create.js
@@ -1,6 +1,8 @@
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import Link from "next/link";
+import Form from "@/components/Form";
+import Comments from "@/components/Comments";
 
 export default function CreatePlacePage() {
   const router = useRouter();
@@ -21,6 +23,8 @@ export default function CreatePlacePage() {
   return (
     <>
       <h2 id="add-place">Add New Place</h2>
+      <Form />
+
       <Link href="/">Go Back</Link>
     </>
   );
