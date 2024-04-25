b74b868f437bc23550bdad4f1f12d97c9d641017
OLENA DOBREVA <sectionone@ukr.net>
2024-04-25 15:25:36 +0200

    generate new api key and replace with variable

--- utils/googleMapsLoader.js
+++ utils/googleMapsLoader.js
@@ -1,7 +1,7 @@
 import { Loader } from "@googlemaps/js-api-loader";
 
 const loader = new Loader({
-  apiKey: "AIzaSyDno6v-E50a4gpxcn3mrKhk33ElYl5jfls",
+  apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
   version: "weekly",
   libraries: ["places", "marker"],
 });
